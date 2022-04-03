3d8a1766bc275ffb638ae47e983e1da111b8e4ef
Mx <6020748+maxmadell@users.noreply.github.com>
2022-04-03 05:10:48 +0100

    - ReactShareViewController.swift 0.68.0 modification

--- ios/ReactShareViewController.swift
+++ ios/ReactShareViewController.swift
@@ -11,8 +11,7 @@ import RNShareMenu
 class ReactShareViewController: ShareViewController, RCTBridgeDelegate, ReactShareViewDelegate {
   func sourceURL(for bridge: RCTBridge!) -> URL! {
 #if DEBUG
-    return RCTBundleURLProvider.sharedSettings()?
-      .jsBundleURL(forBundleRoot: "index.share", fallbackResource: nil)
+    return RCTBundleURLProvider.sharedSettings()?.jsBundleURL(forBundleRoot: "index.share")
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
