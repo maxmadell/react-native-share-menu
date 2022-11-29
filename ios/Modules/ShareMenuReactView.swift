11a42e2b83b69e604f1e787508825ee2631d0109
Mx <6020748+maxmadell@users.noreply.github.com>
2022-11-29 00:28:06 +0000

    - Remove attachViewDelegate guard (fixes crash issue on ios device)

--- ios/Modules/ShareMenuReactView.swift
+++ ios/Modules/ShareMenuReactView.swift
@@ -17,8 +17,6 @@ public class ShareMenuReactView: NSObject {
     }
 
     public static func attachViewDelegate(_ delegate: ReactShareViewDelegate!) {
-        guard (ShareMenuReactView.viewDelegate == nil) else { return }
-
         ShareMenuReactView.viewDelegate = delegate
     }
 
